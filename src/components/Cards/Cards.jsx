fa21d18b905e62fa7dfc45e307bd115d035af426
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-07 13:56:44 +0100

    Fetch data from api and pass to Cards component as props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,8 @@
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import React from 'react'
 
-const Cards = () => {
+const Cards = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>Cards</h1>
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Chart = () => {
   return (
     <div>
-      <h2>Chart</h2>
+      <h1>Chart</h1>
     </div>
   )
 }
